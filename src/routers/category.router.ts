01d507dde35e84a5b934d4efcf2feaab8138586b
Bedo <bedonassef71@gmail.com>
2024-04-06 10:14:19 +0200

    apply auth

--- src/routers/category.router.ts
+++ src/routers/category.router.ts
@@ -4,6 +4,7 @@ import { CategoryController } from '../controllers/category.controller';
 import { createCategoryPipe } from '../utils/pipes/categories/create-category.pipe';
 import { deleteCategoryPipe } from '../utils/pipes/categories/delete-category.pipe';
 import { updateCategoryPipe } from '../utils/pipes/categories/update-category.pipe';
+import { isAuthMiddleware } from '../utils/middlewares/is-auth.middleware';
 
 export const router: Router = Router();
 
@@ -12,13 +13,13 @@ const categoryController: CategoryController = new CategoryController(new Catego
 router
   .route('/')
   .get(categoryController.findAll.bind(categoryController))
-  .post(createCategoryPipe, categoryController.create.bind(categoryController));
+  .post(isAuthMiddleware, createCategoryPipe, categoryController.create.bind(categoryController));
 
 router.route('/:slug').get(categoryController.findOne.bind(categoryController));
 
 router
   .route('/:id')
-  .patch(updateCategoryPipe, categoryController.update.bind(categoryController))
-  .delete(deleteCategoryPipe, categoryController.delete.bind(categoryController));
+  .patch(isAuthMiddleware, updateCategoryPipe, categoryController.update.bind(categoryController))
+  .delete(isAuthMiddleware, deleteCategoryPipe, categoryController.delete.bind(categoryController));
 
 router.route('/:slug/subcategories').get(categoryController.findSubcategoriesBySlug.bind(categoryController));
--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -5,12 +5,13 @@ import { Payload } from '../utils/dtos/auth/payload.dto';
 export class TokenService {
   private SECRET_KEY: string = '';
   private EXPIRES_IN: string | number = process.env.EXPIRES_IN || '1h';
+
   generate(payload: Payload): string {
     this.SECRET_KEY = getSecretKey();
     return jwt.sign(payload, this.SECRET_KEY, { expiresIn: this.EXPIRES_IN });
   }
 
-  verify(token: string) {
+  verify(token: string): any | null {
     try {
       return jwt.verify(token, this.SECRET_KEY);
     } catch (error) {
