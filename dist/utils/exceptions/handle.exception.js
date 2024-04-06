52376c462d24849538cd17d2529ffb44f4df2ff5
Bedo Nassef <77593910+BedoNassef71@users.noreply.github.com>
2024-04-06 08:05:31 +0200

    Delete dist directory

--- dist/utils/exceptions/handle.exception.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleException = void 0;
-const handleException = (err, req, res) => {
-    console.error(err.stack);
-    res.status(500).send({ message: 'Something broke!' });
-};
-exports.handleException = handleException;
