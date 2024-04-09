f04be031f7bc7b656c51289518fbac37b3cf3b29
Bedo <bedonassef71@gmail.com>
2024-04-09 09:43:43 +0200

    implement seeders

---
+++ src/utils/db-seed/code/insert/insert-categories.ts
@@ -0,0 +1,17 @@
+import { categoryService } from '../../ioc/services.ioc';
+import { CreateCategoryDto } from '../../dtos/category/create-category.dto';
+import {readFile } from './common'
+
+export const insertCategories = async () => {
+  try {
+
+    const categoriesEntries: CreateCategoryDto[] = readFile('categories.json');
+
+    for (const category of categoriesEntries) {
+      await categoryService.create(category);
+    }
+    console.log('Categories data inserted successfully');
+  } catch (error: any) {
+    console.error('Error inserting Categories data:', error.message);
+  }
+};
---
+++ src/utils/db-seed/code/insert/insert-products.ts
@@ -0,0 +1,17 @@
+import {readFile } from './common'
+import { CreateProductDto } from '../../dtos/product/create-product.dto';
+import { productService } from '../../ioc/services.ioc';
+
+export const insertProducts = async () => {
+  try {
+
+    const productsEntries: CreateProductDto[] = readFile('products.json');
+
+    for (const product of productsEntries) {
+      await productService.create(product);
+    }
+    console.log('Products data inserted successfully');
+  } catch (error: any) {
+    console.error('Error inserting Products data:', error.message);
+  }
+};
